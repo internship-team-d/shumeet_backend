7dbb6bc895c8dfa72a5b047fa8a832cb18c0e0f6
fukamikensuke <kensuke8293@gmail.com>
2022-09-23 19:51:58 +0900

    feat :ログイン実装

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import *
-app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    return "Hello World!"
- 
-if __name__ == "__main__":
-    app.run()
+from fastapi import *
+import cruds.user_db_create as user_db_create
+import sqlite3
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    conn = sqlite3.connect("user.db")
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, username STRING,password STRING)')
+    cur.execute('CREATE TABLE chat(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id1 INTEGER,user_id2 INTEGER,room STRING)')
+    cur.execute('CREATE TABLE chatmess(id INTEGER PRIMARY KEY AUTOINCREMENT, chat_id INTEGER,to_user INTEGER,from_user INTEGER,message STRING)')
+    conn.commit()
+
+    return {"message": "Hello World"}
+
+@app.post("/login/")
+async def login(username: str = Form(), password: str = Form()):
+    user_db_create(username,password)
+    return True
+
+
+
