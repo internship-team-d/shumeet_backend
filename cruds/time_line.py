9e6d9b748fc2d2f4cbf1ef284ae0a91b55205b4a
fukamikensuke <kensuke8293@gmail.com>
2022-09-24 12:49:41 +0900

    fix : from対応

---
+++ cruds/time_line.py
@@ -0,0 +1,5 @@
+from doctest import testsource
+import sqlite3   
+
+def list_get():
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import A
 from fastapi import *
 import cruds.user_db as user_db
 import cruds.dm as dm
@@ -5,12 +6,19 @@ import sqlite3
 from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-#   conn = sqlite3.connect("user.db")
-#     cur = conn.cursor()
-#     cur.execute('CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, username STRING,password STRING)')
-#     cur.execute('CREATE TABLE chat(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id1 INTEGER,user_id2 INTEGER,room STRING)')
-#     cur.execute('CREATE TABLE chatmess(id INTEGER PRIMARY KEY AUTOINCREMENT, chat_id INTEGER,to_user INTEGER,from_user INTEGER,message STRING)')
-#     conn.commit()
+# conn = sqlite3.connect("user.db")
+# cur = conn.cursor()
+# cur.execute('DROP TABLE IF EXISTS user_list')
+# cur.execute('DROP TABLE IF EXISTS profile_list')
+# cur.execute('DROP TABLE IF EXISTS chat')
+# cur.execute('DROP TABLE IF EXISTS chatmess')
+
+# cur.execute('CREATE TABLE user_list(id INTEGER PRIMARY KEY AUTOINCREMENT, username STRING,password STRING)')
+# cur.execute('CREATE TABLE profile_list(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER,username STRING,birthday STRING,comment STRING)')
+# cur.execute('CREATE TABLE chat(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id1 INTEGER,user_id2 INTEGER,room STRING)')
+# cur.execute('CREATE TABLE chatmess(id INTEGER PRIMARY KEY AUTOINCREMENT, chat_id INTEGER,to_user INTEGER,from_user INTEGER,message STRING)')
+# cur.execute('CREATE TABLE time_list(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER,text STRING)')
+# conn.commit()
 
 app.add_middleware(
   CORSMiddleware,
@@ -24,8 +32,8 @@ app.add_middleware(
 def root():
     return {"message": "Hello World"}
 
-@app.post("/login/{username}/{password}")#ユーザー登録　名前、パスワードの登録
-def login(username: str , password: str):
+@app.post("/login/")#ユーザー登録　名前、パスワードの登録
+def login(username: str = Form() , password: str =Form()):
     name = user_db.regist(username,password)
     return {"message":name} #ユーザー名が返ってくる
 
@@ -35,8 +43,8 @@ def list_get():
     list = user_db.list_get()
     return list
 
-@app.get("/userdata/{username}")#ユーザーデータの出力（必要な値：ユーザー名）
-def user_get(username :str):
+@app.post("/userdata/")#ユーザーデータの出力（必要な値：ユーザー名）
+def user_get(username :str = Form()):
     output = []
     output = user_db.user_data(username)
     return {"message":output} #ユーザーid,名前、パスワードが出てくる
@@ -52,9 +60,12 @@ def chat_get(myid:int,chatid:int):
     output = dm.message_room(myid,chatid)
     return output # 相手のid,自身のid,過去のメッセージ内容,自身の名前が返ってくる
 
-@app.get("/text/{myid}/{chatid}/{get_text}") #メッセージ送信(必要な値：自身のid,チャットのルームid,メッセージ)
-def chat_post(myid:int,chatid:int,get_text:str):
+@app.post("/text/{myid}/{chatid}/") #メッセージ送信(必要な値：自身のid,チャットのルームid,メッセージ)
+def chat_post(myid:int,chatid:int,get_text:str = Form()):
     message = dm.message_text(myid,chatid,get_text)
     return message #送ったメッセージが返ってくる
 
-
+@app.get("timeline/")
+def time_line_get():
+    list = A
+    return list
